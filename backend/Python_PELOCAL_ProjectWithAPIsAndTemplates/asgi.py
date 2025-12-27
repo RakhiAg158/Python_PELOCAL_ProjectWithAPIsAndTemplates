01ccfd5be00587053c772c649a650858788d5897
Rakhi Agarwal <rakhiagarwal@Rakhis-MacBook-Air.local>
2025-12-28 00:08:41 +0530

    Initial Django project setup

---
+++ backend/Python_PELOCAL_ProjectWithAPIsAndTemplates/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for Python_PELOCAL_ProjectWithAPIsAndTemplates project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "Python_PELOCAL_ProjectWithAPIsAndTemplates.settings"
+)
+
+application = get_asgi_application()
